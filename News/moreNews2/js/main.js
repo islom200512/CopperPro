1739a107c257a5d955a05950902a2c0a94cd346f
islom200512 <101330876+islom200512@users.noreply.github.com>
2022-04-18 14:03:33 +0500

    hello

---
+++ News/moreNews2/js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: true,
+
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        626: {
+            items: 1
+        },
+        900: {
+            items: 1
+        }
+    }
+})
